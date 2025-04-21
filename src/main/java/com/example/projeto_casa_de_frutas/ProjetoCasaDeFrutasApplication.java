3ac9da7bc9a65922f5f7a90d067a57a206c12578
RyanLudieri <ryan.evangelista@sou.unifal-mg.edu.br>
2025-04-21 15:30:27 -0300

    first commit

---
+++ src/main/java/com/example/projeto_casa_de_frutas/ProjetoCasaDeFrutasApplication.java
@@ -0,0 +1,13 @@
+package com.example.projeto_casa_de_frutas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCasaDeFrutasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCasaDeFrutasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projeto_casa_de_frutas/ProjetoCasaDeFrutasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projeto_casa_de_frutas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCasaDeFrutasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
